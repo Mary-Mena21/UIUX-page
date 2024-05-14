a2301aa2759bdfb17692cdcd317a1b69d907971a
Mariana Mena <maranashokry@ahoo.com>
2024-05-13 22:55:28 -0500

    style form

--- src/SocialMediaLogin.jsx
+++ src/SocialMediaLogin.jsx
@@ -1,11 +1,13 @@
 import './SocialMediaLogin.css';
-import { FaFacebookF, FaTwitter } from 'react-icons/fa';
+import { FaFacebookF } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
+//import './Button.css';
+import './AuthForm.css';
 
 function SocialMediaLogin() {
   return (
     <div className="social-media-login">
-      <button onClick={() => alert('Login with Facebook')}>
+      <button  onClick={() => alert('Login with Facebook')}>
         {' '}
         <FaFacebookF
           style={{ color: '#395693', fontSize: '15px', textAlign: 'center' }}
